c11792387c4ff6da7330f58db7f6360abb6e710e
Devon Dalrymple <devonxpersonal@gmail.com>
2023-06-20 19:10:59 -0400

    Added TailwindCSS and two basic mind maps for planning the frontend

---
+++ Software/Portfolio/Portfolio/ClientApp/tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,scss,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
