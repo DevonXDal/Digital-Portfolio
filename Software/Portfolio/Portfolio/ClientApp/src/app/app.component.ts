64aa59016efa8f9d1f37b63b569073177fb6ab33
Devon Dalrymple <devonxpersonal@gmail.com>
2023-07-12 14:37:15 -0400

    Added rough listing as to what is planned to be tested. styled the tabview for the summary page About Me section. Improved tracking for theme and renamed it to uiMode in the app component.

--- Software/Portfolio/Portfolio/ClientApp/src/app/app.component.ts
+++ Software/Portfolio/Portfolio/ClientApp/src/app/app.component.ts
@@ -4,14 +4,32 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  public theme: 'light' | 'dark' = 'light';
+  public uiMode: 'light' | 'dark' = 'light';
 
   constructor() {}
 
-  /// Toggles between light and dark mode theme
+  /// Prepares the application by determining whether to use light mode or dark mode.
+  ngOnInit(): void {
+      let savedUiMode: string | null = localStorage.getItem('preferred-theme');
+
+      // Help from Medium article: https://pkief.medium.com/automatic-dark-mode-detection-in-angular-material-8342917885a0
+      // Sees if the user has selected a theme, if they haven't, check if dark mode preference is set on their browser, 
+      // finally default to light mode.
+      if (savedUiMode && (savedUiMode === 'light' || savedUiMode === 'dark')) {
+        this.uiMode = savedUiMode!;
+      } else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark").matches) {
+        this.uiMode = 'dark';
+      } else {
+        this.uiMode = 'light';
+      }
+  }
+
+  /// Toggles between light and dark mode theme and saves the current option
   toggleTheme() {
-    this.theme = (this.theme === 'light') ? 'dark' : 'light';
+    this.uiMode = (this.uiMode === 'light') ? 'dark' : 'light';
+
+    localStorage.setItem('preferred-theme', this.uiMode); // May throw error to console if storage is disabled
   }
 }
