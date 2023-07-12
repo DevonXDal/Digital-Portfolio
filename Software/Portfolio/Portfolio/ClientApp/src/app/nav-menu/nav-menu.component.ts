1d8ee6fb7937156c5f5e0379753bcf5e7a62c27b
Devon Dalrymple <devonxpersonal@gmail.com>
2023-07-12 17:29:45 -0400

    Fixed misaligned top row of navbar, sets isExpanded for navbar to false when screen size changes to a large screen's width. Centered and made the buttons wider on the mobile navbar.

--- Software/Portfolio/Portfolio/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Software/Portfolio/Portfolio/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,9 +7,17 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
   public isExpanded = false;
+  
+  constructor() {}
 
-  /// Toggles whether the navbar is expanded or not on mobile devices
+  /// Toggles whether the navbar is expanded or not on mobile devices.
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  /// Listens for changes in screen size and sets isExpanded to false if the screen is a large or greater screen.
+  @HostListener('window:resize', ['$event'])
+  checkScreenSize() {
+    if (window.innerWidth >= 1024) this.isExpanded = false;
   }
 }
