8d67a493de81aaf80159991347b173e434d5fb72
Devon Dalrymple <devonxpersonal@gmail.com>
2023-07-26 15:30:14 -0400

    I decided that I did not want the previous projects page, I instead wanted to focus on its corresponding GitHub page. Thus, this requirement was removed and related content.

--- Software/Portfolio/Portfolio/ClientApp/src/app/other-content/other-content.module.ts
+++ Software/Portfolio/Portfolio/ClientApp/src/app/other-content/other-content.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { IntegratedProjectsComponent } from './integrated-projects/integrated-projects.component';
 import { UpdateListComponent } from './update-list/update-list.component';
 import { RouterModule } from '@angular/router';
-import { PreviousProjectsComponent } from './previous-projects/previous-projects.component';
 import {DockModule} from 'primeng/dock';
 import {MenubarModule} from 'primeng/menubar';
 
@@ -12,15 +11,13 @@ let base = 'other-content';
 @NgModule({
   declarations: [
     IntegratedProjectsComponent,
-    UpdateListComponent,
-    PreviousProjectsComponent
+    UpdateListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: `${base}`, redirectTo: `${base}/updates`, pathMatch: 'full' },
       { path: `${base}/integrated-projects`, component: IntegratedProjectsComponent},
-      { path: `${base}/previous-projects`, component: PreviousProjectsComponent},
       { path: `${base}/updates`, component: UpdateListComponent },
     ]),
     DockModule,
